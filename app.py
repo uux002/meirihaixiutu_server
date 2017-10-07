9b39d4e2e67664d7de83b92c4204907f5ba2956b
uux002 <uux002@gmail.com>
2017-10-08 00:56:08 +0800

    Update

--- app.py
+++ app.py
@@ -5,26 +5,15 @@ from aiohttp import web
 import hashlib
 
 
-async def index(request):
+def index(request):
 	echostr = 'success'
 	try:
-		signature = request.query['signature']
-		timestamp = request.query['timestamp']
-		nonce = request.query['nonce']
 		echostr = request.query['echostr']
-		token = 'jkilopqcv0968'
-		list = [token,timestamp,nonce]
-		list.sort()
-		sha1 = hashlib.sha1()
-		map(sha1.update,list)
-		hashcode = sha1.hexdigest()
-		logging.info("handle/GET func: hashcode, signature:",hashcode,signature)
-		if hashcode == signature:
-			return echostr
-		else:
-			return ""
 	except Exception as ex:
-		return ex
+		pass
+
+	logging.info("Echo STR:" + echostr)
+	return web.Response(body=echostr.encode('utf-8'))
 
 	#return web.Response(body=echostr.encode('utf-8'))
 
@@ -33,14 +22,14 @@ async def index(request):
 	#resp.content_type = 'text/html;charset=utf-8'
 	#return resp
 
-async def postWX(request):
+def postWX(request):
 	pass
 
 @asyncio.coroutine
 def init(loop):
 	app = web.Application(loop=loop)
-	app.router.add_route('GET','/',index)
-	app.router.add_route('POST','/',postWX)
+	app.router.add_route('GET','/wx',index)
+	app.router.add_route('POST','/wx',postWX)
 	srv = yield from loop.create_server(app.make_handler(),'127.0.0.1',7000)
 	logging.info('Server started at http://127.0.0.1:7000...')
 	return srv
