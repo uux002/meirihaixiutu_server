72b397870357fc132ead46c8fc97a3e07fccc100
uux002 <uux002@gmail.com>
2017-10-08 10:49:01 +0800

    Update

--- app.py
+++ app.py
@@ -10,22 +10,48 @@ from aiohttp import web
 import hashlib
 import xml.etree.ElementTree as ET
 import time
+import random
+
+auto_text_reply = [
+	'求你了老公,我不嘛',
+	'人家以后再也不理你了啦!',
+	'别这样啦，人家是个女孩子嘛!',
+	'别这样啦，之前你不是这个样子的嘛!',
+	'不嘛不嘛，我要我要',
+	'人家就是想要你多关心一点!',
+	'就要就要，人家就要，不给就不理你了',
+	'你坏哦! 我要告诉妈妈说你欺负女孩子!',
+	'你好讨厌哦!',
+	'没人跟你一起啊! 我陪你吧!',
+	'你今天有没有想念人家呀!',
+	'人家不开心',
+	'你好坏哦，欺负人家，哼!',
+	'老公，帮我拎着包裹，你看人家的小手，都勒出红印来了',
+	'老公，我累了，你去做饭吧，求求你了!',
+	'看到你心情就特别好!',
+	'一天没和你聊天，就觉得哪里不对劲!',
+	'不要这样嘛!',
+	'你才傻瓜呢!',
+	'快亲亲人家啦!',
+	'我家宝贝就这样，嘿嘿',
+	'如果我不在这了，你会喜欢别的女孩么!',
+	'想你想的睡不着',
+	'别生气了，我错了还不行呀~'
+]
+
+def get_random_text_reply_content():
+	count = len(auto_text_reply)
+	selected_index = random.randint(0,count - 1)
+	return auto_text_reply[selected_index]
+
 
 def parse_xml(xmlData):
 	ToUserName = xmlData.find('ToUserName').text
 	FromUserName = xmlData.find('FromUserName').text
 	CreateTime = xmlData.find('CreateTime').text
 	MsgType = xmlData.find('MsgType').text
-	Content = xmlData.find('Content').text
-	MsgId = xmlData.find('MsgId').text
-	#print(ToUserName,FromUserName,CreateTime,MsgType,Content,MsgId)
-	return ToUserName,FromUserName,CreateTime,MsgType,Content,MsgId
-
-def parse_subscribe_xml(xmlData):
-	ToUserName = xmlData.find('ToUserName').text
-	FromUserName = xmlData.find('FromUserName').text
-	CreateTime = xmlData.find('CreateTime').text
-	MsgType = xmlData.find('MsgType').text
+	#Content = xmlData.find('Content').text
+	#MsgId = xmlData.find('MsgId').text
 	#print(ToUserName,FromUserName,CreateTime,MsgType,Content,MsgId)
 	return ToUserName,FromUserName,CreateTime,MsgType
 
@@ -66,12 +92,12 @@ async def postWX(request):
 	info = await request.text()
 	logging.info("收到post请求:" + info)
 	xmlData = ET.fromstring(info)
+	ToUserName, FromUserName,CreateTime,MsgType = parse_xml(xmlData)
 	
-
 	result = 'success'
 	if MsgType.lower() == 'text':
-		ToUserName, FromUserName,CreateTime,MsgType,Content,MsgId = parse_xml(xmlData)
-		msg = get_text_reply_xml(FromUserName,ToUserName,"主人，我爱你噢")
+		content = get_random_text_reply_content()
+		msg = get_text_reply_xml(FromUserName,ToUserName,content)
 		return web.Response(body=msg.encode('utf-8'))
 	elif MsgType.lower() == 'voice':
 		pass
@@ -80,8 +106,7 @@ async def postWX(request):
 		Event = re.findall(reg, info)[0]
         # hu 接收事件推送（关注、取消关注等等）
 		if Event.lower() == 'subscribe':       # hu 用户关注事件
-			ToUserName, FromUserName,CreateTime,MsgType = parse_subscribe_xml(xmlData)
-			msg = get_text_reply_xml(FromUserName,ToUserName,"主人，欢迎你来到每日害羞图")
+			msg = get_text_reply_xml(FromUserName,ToUserName,"主人，欢迎你来到每日害羞图，我会好好爱你的，嘿嘿")
 			return web.Response(body=msg.encode('utf-8'))
 		elif Event.lower() == 'unsubscribe':  # hu 取消关注事件
 			pass
